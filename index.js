d44912c6140f57f12d472ae9670e729b5fb50cab
Leon <leonmuigaike@gmail.com>
2026-04-25 22:30:53 +0300

    Add JavaScript scope lab project

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Lab: Flatburger Restaurant
+let burgers = ["Hamburger", "Cheeseburger"];
+let drink = "milkshake";
+let featuredItem = "";
+
+function setFeaturedItem(item) {
+    featuredItem = item;
+}
+
+function addSpecialBurger() {
+    if (true) {
+        let specialBurger = "BBQ Bacon Burger";
+        burgers.push(specialBurger);
+    }
+}
+
+setFeaturedItem("Double Cheeseburger");
+addSpecialBurger();
+
+console.log(burgers);
+console.log(drink);
+console.log(featuredItem);
